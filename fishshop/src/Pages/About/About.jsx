de80dd7063fc1afb1237c992dc7dcdea6d613e50
Rajendra <itsrajendrapatel@gmail.com>
2023-09-29 18:13:49 +0530

    About page Done

--- fishshop/src/Pages/About/About.jsx
+++ fishshop/src/Pages/About/About.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="main-about-div">
+      <div className="background-image">
+        <div class="content">
+          <h1>Your Story</h1>
+          <p>Fresh from the Ocean to Your Plate!</p>
+        </div>
+      </div>
 
-export default About
+      <div className="first-about-div">
+        <img src={require("../../Assets/client.jpg")} alt="client" />
+        <div>
+          <p>How it all Started</p>
+          <h1>Inspired by the pearl diving heritage of the UAE</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, nisi
+            dolor at impedit repudiandae officiis. Numquam illum ducimus sequi
+            sint ipsum recusandae aspernatur vitae sapiente.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+            impedit hic excepturi, cum doloribus, optio explicabo dolores sint
+            eaque architecto odio at itaque aliquid pariatur velit temporibus
+            vel officia autem quisquam rerum. Fuga, recusandae sapiente
+            perspiciatis minima placeat voluptate harum officia quaerat officiis
+            ratione impedit eum consequuntur totam incidunt quos.
+          </p>
+        </div>
+      </div>
+      <div className="second-about-div">
+        <div class="contents">
+          <h1>Our mission</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolorem, illum ad quibusdam aliquam quae ipsum assumenda corporis voluptatem nostrum beatae excepturi quam, alias eaque quasi aliquid! Molestias, minus ipsa.
+          </p>
+        </div>
+        <div class="contents">
+          <h1>Our Vision</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, fuga
+            non sit laborum nam fugit necessitatibus doloribus voluptate iusto
+            corporis reiciendis. Repellat id aliquam soluta dicta dolorum
+            doloremque dolorem ex.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- fishshop/src/Pages/Contact/Contact.jsx
+++ fishshop/src/Pages/Contact/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="main-contact-div">
+      <div className="background-image-contact">
+        <div class="content">
+          <h1>Contact Us</h1>
+          <p>Give us a call, send an email, we are always there for you...</p>
+        </div>
+      </div>
 
-export default Contact
+      <p className="contact-para">
+        Let us know your requirements and we are happy to help. Whether you are
+        a wholesale retailer, a supermarket chain, a restaurant or just anyone
+        in need of a reliable supplier, we can offer you the best of pole and
+        line frozen and fresh tuna from the Maldives.
+      </p>
+
+      <div className="contact-input-div">
+        <div class="input-container">
+          <input type="text" id="inputField" class="custom-input" />
+          <label for="inputField" class="input-label">
+            Your Label
+          </label>
+          <div class="input-border"></div>
+        </div>
+        <div class="input-container">
+          <input type="text" id="inputField" class="custom-input" />
+          <label for="inputField" class="input-label">
+            Your Label
+          </label>
+          <div class="input-border"></div>
+        </div>
+        <div class="input-container">
+          <input type="text" id="inputField" class="custom-input" />
+          <label for="inputField" class="input-label">
+            Your Label
+          </label>
+          <div class="input-border"></div>
+        </div>
+        <div class="input-container">
+          <input type="text" id="inputField" class="custom-input" />
+          <label for="inputField" class="input-label">
+            Your Label
+          </label>
+          <div class="input-border"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
