df8599407bb92f81119903ae93645dc8a7f18616
Rajendra <itsrajendrapatel@gmail.com>
2023-09-29 20:52:02 +0530

    Contact Page Done

--- fishshop/src/Pages/Contact/Contact.jsx
+++ fishshop/src/Pages/Contact/Contact.jsx
@@ -1,54 +1,53 @@
 import React from "react";
 import "./Contact.css";
+import { message } from "antd";
 
 const Contact = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "We will contact you soon...",
+    });
+  };
+
   return (
-    <div className="main-contact-div">
-      <div className="background-image-contact">
-        <div class="content">
-          <h1>Contact Us</h1>
-          <p>Give us a call, send an email, we are always there for you...</p>
+    <>
+      {contextHolder}
+      <div className="main-contact-div">
+        <div className="background-image-contact">
+          <div class="content">
+            <h1>Contact Us</h1>
+            <p>Give us a call, send an email, we are always there for you...</p>
+          </div>
+        </div>
+
+        <p className="contact-para">
+          Let us know your requirements and we are happy to help. Whether you
+          are a wholesale retailer, a supermarket chain, a restaurant or just
+          anyone in need of a reliable supplier, we can offer you the best of
+          pole and line frozen and fresh tuna from the Maldives.
+        </p>
+
+        <div className="contact-input-div">
+          <div className="first-input-div">
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="second-input-div">
+            <input type="text" placeholder="Contact No" />
+            <input type="text" placeholder="What you are looking For ?" />
+          </div>
+          <div className="third-input-div">
+            <textarea type="text" placeholder="Message" rows="10" cols="85" />
+          </div>
+          <button class="custom-button" onClick={success}>
+            Contact Me
+          </button>
         </div>
       </div>
-
-      <p className="contact-para">
-        Let us know your requirements and we are happy to help. Whether you are
-        a wholesale retailer, a supermarket chain, a restaurant or just anyone
-        in need of a reliable supplier, we can offer you the best of pole and
-        line frozen and fresh tuna from the Maldives.
-      </p>
-
-      <div className="contact-input-div">
-        <div class="input-container">
-          <input type="text" id="inputField" class="custom-input" />
-          <label for="inputField" class="input-label">
-            Your Label
-          </label>
-          <div class="input-border"></div>
-        </div>
-        <div class="input-container">
-          <input type="text" id="inputField" class="custom-input" />
-          <label for="inputField" class="input-label">
-            Your Label
-          </label>
-          <div class="input-border"></div>
-        </div>
-        <div class="input-container">
-          <input type="text" id="inputField" class="custom-input" />
-          <label for="inputField" class="input-label">
-            Your Label
-          </label>
-          <div class="input-border"></div>
-        </div>
-        <div class="input-container">
-          <input type="text" id="inputField" class="custom-input" />
-          <label for="inputField" class="input-label">
-            Your Label
-          </label>
-          <div class="input-border"></div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
